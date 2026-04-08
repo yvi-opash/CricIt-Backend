0b34e94e9a10edcc5bfbee5e93310653c8ba9bc0
yvi-opash <yvi.opash@gmail.com>
2026-04-08 17:39:57 +0530

    db2

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,15 +1,16 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const mongoDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URL || '')
-         dbName: "cricitDB"
-        console.log(`MongoDB Connected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL || "", {
+      dbName: "cricitDB", 
+    });
 
-}
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+};
