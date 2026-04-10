e1bb5c405d56e70637c7fc5af7ff0976f723d250
yvi-opash <yvi.opash@gmail.com>
2026-04-10 18:36:30 +0530

    db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,32 +1,32 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// dotenv.config();
-
-// export const mongoDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL || "", {
-//       // dbName: "cricItDB",
-//       // dbName: "cricit", 
-//     });
-
-//     console.log("MongoDB Connected ✅");
-//   } catch (error) {
-//     console.log("Error connecting to MongoDB:", error);
-//   }
-// };
-
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-
-dotenv.config()
+dotenv.config();
 
 export const mongoDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URL || '')
-        console.log(`MongoDB Connected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL || "", {
+      // dbName: "cricItDB",
+      // dbName: "cricit", 
+    });
 
-}
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+};
+
+// import mongoose from 'mongoose'
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// export const mongoDB = async () => {
+//     try{
+//         const conn = await mongoose.connect(process.env.MONGO_URL || '')
+//         console.log(`MongoDB Connected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+//     } catch (error) {
+//         console.log('Error connecting to MongoDB:', error)
+//     }
+
+// }
